7c206749df48bea4a72c6a233a54950d70238a07
marutoo ozmaky <marawanraafat999@gmal.com>
2024-04-27 22:50:15 +0200

    add files

---
+++ script.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id: "123456789",
+        title: "Red T-Shirt",
+        image: "./images/red.png",
+        priceBeforeSale: 150,
+        priceAfterSale: 120,
+        amount: 1,
+    },
+    {
+        id: "456789123",
+        title: "Black T-Shirt",
+        image: "./images/black.png",
+        priceBeforeSale: 180,
+        priceAfterSale: 130,
+        amount: 1,
+    },
+    {
+        id: "789123456",
+        title: "Blue T-Shirt",
+        image: "./images/blue.png",
+        priceBeforeSale: 200,
+        priceAfterSale: 160,
+        amount: 1,
+    },
+]
