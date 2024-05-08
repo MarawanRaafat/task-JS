0aac0a70a47fe05da2d5769d3b470d8e62795422
marutoo ozmaky <marawanraafat999@gmal.com>
2024-05-08 18:40:53 +0200

    updatefiles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const products = [
     {
         id: "123456789",
@@ -23,8 +24,53 @@ const products = [
         priceAfterSale: 160,
         amount: 1,
     },
-]
+];
+
+const productsContainer = document.querySelector(".section");
+
+products.forEach(product => {
+    productsContainer.innerHTML += `
+
+      <div class="stor-item">
+
+        <div class="left-site">
+
+            <div class="img">
+                <img src=${product.image} alt="#">
+            </div>
+
+            <div class="text">
+                <label>${product.title}</label>
+
+                <div class="price">
+                    <span class="green">${product.priceAfterSale}</span>
+                    <span class="red">${product.priceBeforeSale}</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="right-side">
+            <button class="del-item"> <i class="fa-solid fa-xmark"></i></button>
+            <div class="counter">
+                <button class="minus-btn"><i class="fa-solid fa-minus"></i></button>
+                <span class="number">${product.amount}</span>
+                <button class="plus-btn"><i class="fa-solid fa-plus"></i></button>
+            </div>
+        </div>
+        </div>
+        `;
+});
 
 
+
+const changeNumber = document.querySelector(".number").textContent
+const plus = document.querySelector(".plus-btn").onclick;
+const minus = document.querySelector(".minus-btn");
+
+
+// plus.forEach(mathPlus , function randSumSub(changeNumber, ) {
+//    result = changeNumber + 1 ;
+//    return result;
+// })
 
 
